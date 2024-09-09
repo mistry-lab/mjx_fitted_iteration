21bc2a08bbcfbf533871d1083becb17854c55e2a
thomascbrs <thomas.corberes@gmail.com>
2024-09-09 19:54:13 +0100

    VErification PD controller

--- runner.py
+++ runner.py
@@ -5,14 +5,18 @@ from mujoco import mjx
 import jax
 import wandb
 from config.cps import ctx as cp_ctx
+from config.di import ctx as di_ctx
 from simulate import controlled_simulate
 from trainer import gen_targets_mapped, make_step, loss_fn
 import equinox as eqx
 import optax
 import copy
+import numpy as np
+from utils.mj_vis import animate_trajectory
 
 wandb.init(project="fvi", anonymous="allow")
-configs = {"cartpole_swing_up": cp_ctx}
+configs = {"cartpole_swing_up": cp_ctx, "double_integrator":di_ctx }
+
 
 if __name__ == '__main__':
     try:
@@ -25,23 +29,37 @@ if __name__ == '__main__':
         optim = optax.adamw(ctx.cfg.lr)
         opt_state = optim.init(eqx.filter(ctx.cbs.net, eqx.is_array))
 
+        # Random number generator
+        init_key = jax.random.PRNGKey(ctx.cfg.seed)
+        key, subkey = jax.random.split(init_key)
+
         with jax.default_device(jax.devices()[0]):
             # Model definition
             model = mujoco.MjModel.from_xml_path(ctx.cfg.model_path)
             model.opt.timestep = ctx.cfg.dt # Setting timestep from Context
+            data = mujoco.MjData(model)
             mx = mjx.put_model(model)
-            sim = eqx.filter_jit(jax.vmap(controlled_simulate, in_axes=(0, None, None, None)))
+            sim = eqx.filter_jit(jax.vmap(controlled_simulate, in_axes=(0, None, None)))
+            # sim = jax.vmap(controlled_simulate, in_axes=(0, None, None))
             f_target = eqx.filter_jit(jax.vmap(gen_targets_mapped, in_axes=(0,0,None)))
             f_make_step = eqx.filter_jit(make_step)
-            net = copy.deepcopy(ctx.cbs.net)
-            for _ in range(10):
-                x_inits = ctx.cbs.init_gen(ctx.cfg.batch, jax.random.PRNGKey(ctx.cfg.seed))
-                time_init = time.time()
-                x, u = sim(x_inits, mx, ctx, net)
+            for e in range(ctx.cfg.epochs):
+                key, xkey, tkey = jax.random.split(key, num = 3)
+                x_inits = ctx.cbs.init_gen(ctx.cfg.batch, xkey)
+                # time_init = time.time()
+                x, u = sim(x_inits, mx, ctx)
                 target  = f_target(x,u, ctx)
-                net, opt_state, loss_value = make_step(optim, ctx.cbs.net,opt_state, loss_fn,x, target)
-                wandb.log({"loss": loss_value, "net": ctx.cbs.net})
-                print(f"Time taken: {time.time() - time_init}")
+                # ctx.cbs.net, opt_state, loss_value = make_step(optim, ctx.cbs.net,opt_state, loss_fn,x, target)
+                # wandb.log({"loss": loss_value, "net": ctx.cbs.net})
+                # print(f"Time taken: {time.time() - time_init}")
+
+                if e % ctx.cfg.vis == 0 and e > 0:
+                    xs = x[jax.random.randint(tkey, (5,), 0, ctx.cfg.nsteps)]
+                    term_cst = ctx.cbs.terminal_cost(xs[...,-1,:])
+                    print(f"Terminal cost is: {term_cst}")
+                    x = np.array(xs.squeeze())
+                    print()
+                    animate_trajectory(x, data, model)
 
     except KeyboardInterrupt:
         print("Exit wandb")
--- simulate.py
+++ simulate.py
@@ -1,9 +1,8 @@
 import jax
 import jax.numpy as jnp
 from mujoco import mjx
-from hjb_controller import ValueFunc
 
-def controlled_simulate(x_inits, mx, ctx, vf):
+def controlled_simulate(x_inits, mx, ctx):
     def set_init(x):
         dx = mjx.make_data(mx)
         qpos = dx.qpos.at[:].set(x[:mx.nq])
@@ -16,10 +15,13 @@ def controlled_simulate(x_inits, mx, ctx, vf):
         act_id = mx.actuator_trnid[:, 0]
         M = mjx.full_m(mx, dx)
         invM = jnp.linalg.inv(M)
-        dvdx = jax.jacrev(vf)(x)
+        dvdx = jax.jacrev(vf)(x) 
         G = jnp.vstack([jnp.zeros_like(invM), invM])
         invR = jnp.linalg.inv(R)
+        # print(f"dvdx.T{dvdx.T[0]}")
         u = (-1/2 * invR @ G.T[act_id, :] @ dvdx.T).squeeze()
+        # u = (-1/2 * dvdx.T).squeeze()
+        # u = jnp.ones_like(u) * 10
         ctrl = dx.ctrl.at[:].set(u)
         dx = dx.replace(ctrl=ctrl)
         return dx
@@ -29,7 +31,7 @@ def controlled_simulate(x_inits, mx, ctx, vf):
         dx = jax.lax.stop_gradient(mjx.step(mx, dx))
         return dx, jnp.concatenate([dx.qpos, dx.qvel, dx.ctrl], axis=0)
 
-    # vf = ctx.cbs.net
+    vf = ctx.cbs.net
     dx = set_init(x_inits)
     _, batched_traj = jax.lax.scan(mjx_step, dx, None, length=ctx.cfg.nsteps)
     x, u = batched_traj[..., :-mx.nu], batched_traj[..., -mx.nu:]
