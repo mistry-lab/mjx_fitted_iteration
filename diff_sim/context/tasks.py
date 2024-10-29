ee313b972684fca5fb52c692d5df4c4b0f595e27
Daniel Layeghi <Daniellayeghi@gmail.com>
2024-10-29 17:55:46 +0000

    Working 3 link arm

--- diff_sim/context/tasks.py
+++ diff_sim/context/tasks.py
@@ -1,8 +1,10 @@
 # from diff_sim.context.cps import ctx as cp_ctx
 from diff_sim.context.di import ctx as di_ctx
-from diff_sim.context.shadow_hand import  ctx as sh_ctx
+from diff_sim.context.shadow_hand import ctx as sh_ctx
+from diff_sim.context.single_arm import ctx as sa_ctx
+
 ctxs = {
     "double_integrator": di_ctx,
-    # "cartpole_swing_up": cp_ctx,
-    "shadow_hand": sh_ctx
+    "shadow_hand": sh_ctx,
+    "single_arm": sa_ctx
 }
