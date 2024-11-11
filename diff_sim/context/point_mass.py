cccd5ddf0b55064e52f0bf07fe3a2cc2e1540b86
Daniel Layeghi <Daniellayeghi@gmail.com>
2024-11-11 16:21:01 +0000

    Working ball push example with qfrcs

--- diff_sim/context/point_mass.py
+++ diff_sim/context/point_mass.py
@@ -9,7 +9,11 @@ from diff_sim.loss_funcs import loss_fn_policy_stoch, loss_fn_td_stoch, loss_fn_
 from diff_sim.context.meta_context import Config, Callbacks, Context
 from diff_sim.nn.base_nn import Network
 
-model_path = os.path.join(os.path.dirname(__file__), '../xmls/point_mass_tendon.xml')
+def op_and_split(f, key):
+    key, subkey = jax.random.split(key)
+    return f(subkey), key
+
+model_path = os.path.join(os.path.dirname(__file__), '../xmls/point_mass.xml')
 def gen_model() -> mujoco.MjModel:
     return mujoco.MjModel.from_xml_path(model_path)
 
@@ -23,7 +27,7 @@ _cfg = Config(
     num_gpu=1,
     dt=0.01,
     ntotal=512,
-    nsteps=32,
+    nsteps=16,
     mx=mjx.put_model(gen_model()),
     gen_model=gen_model
 )
@@ -40,14 +44,11 @@ class Policy(Network):
         self.act = jax.nn.relu
 
     def __call__(self, x, t):
-        # t = t if t.ndim == 1 else t.reshape(1)
-        # x = jnp.concatenate([x, t], axis=-1)
         for layer in self.layers[:-1]:
             x = self.act(layer(x))
         x = self.layers[-1](x).squeeze()
-        x_arm = jnp.tanh(x[:3]) * 0.1
-        ball_control = jnp.concatenate([jnp.tanh(x[3:5]) * 0.1, jnp.tanh(x[5:6]) * 0.01], axis=0)
-        x = jnp.concatenate([x_arm, ball_control * 0, jnp.zeros(3)], axis=0)
+        # tanh the first 2 dimensions with a scale of 0.5 and the last dimension with a scale of 0.1
+        x = jnp.concatenate([jnp.tanh(x[:2]) * 0.1, jnp.tanh(x[2:]) * 0.01], axis=0)
         return x
 
 def policy(net: Network, mx: mjx.Model, dx: mjx.Data, policy_key: jnp.ndarray
@@ -59,13 +60,8 @@ def policy(net: Network, mx: mjx.Model, dx: mjx.Data, policy_key: jnp.ndarray
     return dx, u
 
 def state_encoder(mx: mjx.Model, dx: mjx.Data) -> jnp.ndarray:
-    ball_pos = dx.qpos[3:5]
-    # mocap pos for some reason has one leading dimension (inherent to mujoco)
-    pos_diff = ball_pos - dx.mocap_pos[0, :2]
-    joint_pos = dx.qpos[:3]
-    joint_vel = dx.qvel[:3]
-    ball_vel = dx.qvel[3:5]
-    return jnp.concatenate([joint_pos, pos_diff, ball_pos, joint_vel, ball_vel], axis=0)
+    x = jnp.concatenate([dx.qpos, dx.qvel], axis=0)
+    return x
 
 def state_decoder(x: jnp.ndarray) -> jnp.ndarray:
     return x
@@ -73,48 +69,30 @@ def state_decoder(x: jnp.ndarray) -> jnp.ndarray:
 def control_cost(mx: mjx.Model, dx: mjx.Data) -> jnp.ndarray:
     x = dx.qfrc_applied
     return jnp.dot(
-        x.T, jnp.dot(jnp.diag(jnp.array([0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001])), x)
+        x.T, jnp.dot(jnp.diag(jnp.array([0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001])), x)
     )
 
 def run_cost(mx: mjx.Model, dx: mjx.Data) -> jnp.ndarray:
     x = state_encoder(mx, dx)
-    bound_cost =jnp.maximum(jnp.abs(x[0]) - 2*jnp.pi, 0)
-    state_cost =  jnp.dot(
-        x.T, jnp.dot(jnp.diag(jnp.array([0, 0, 0, 4000, 4000, 0, 0, .0, .0, .0, 1, 1])), x)
+    return jnp.dot(
+        x.T, jnp.dot(jnp.diag(jnp.array([1000, 1000, 1, 0, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1])), x)
     )
-    return state_cost + bound_cost
 
 def terminal_cost(mx: mjx.Model, dx: mjx.Data) -> jnp.ndarray:
     x = state_encoder(mx, dx)
     return _cfg.dt * jnp.dot(
-        x.T, jnp.dot(jnp.diag(jnp.array([0, 0, 0, 4000, 4000, 0, 0, .0, .0, .0, 1, 1])), x)
+        x.T, jnp.dot(jnp.diag(jnp.array([1000, 1000, 1, 0, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.1])), x)
     )
 
 def set_data(mx: mjx.Model, dx: mjx.Data, ctx: Context, key: jnp.ndarray) -> mjx.Data:
-    ang1 = jax.random.uniform(key, (1,), minval=-0, maxval=2*jnp.pi)
-    ang23 = jax.random.uniform(key, (2,), minval=-0.0, maxval=0.0)
-    angs = jnp.concatenate([ang1, ang23], axis=0)
-    l, d_theta = 0.5, 0.25
-
-    _, key = jax.random.split(key)
-    rand_sign = -1. + 2.*jax.random.bernoulli(key, 0.5)
-
-    _, key = jax.random.split(key)
-    l_ball = jax.random.uniform(key, (1,), minval=.2, maxval=.45)
-    obj_x = l_ball * (jnp.sin(angs[0] + d_theta * rand_sign))
-    obj_y = l_ball * (jnp.cos(angs[0] + d_theta * rand_sign))
-
-    obj_z = jnp.array([0.])
-    qpos = jnp.concatenate([angs, obj_x, obj_y, obj_z, jnp.array([1, 0, 0, 0])], axis=0)
-
-    _, key = jax.random.split(key)
-    qvel = jax.random.uniform(key, (_cfg.mx.nv,), minval=-0.1, maxval=0.1)
-
-    _, key = jax.random.split(key)
-    target_pos = jax.random.uniform(key, (2,), minval=-0.5, maxval=0.5)
-
-
-    target_pos = jnp.concatenate([target_pos, jnp.array([0])], axis=0)
+    ball_xy, key = op_and_split(lambda k: jax.random.uniform(k, minval=-0.5, maxval=0.5, shape=(2,)), key)
+    ball_z = jnp.array([0.])
+    ball_quat = jnp.array([1., 0., 0., 0.])
+    ball_vel = jnp.array([0., 0., 0.])
+    ball_ang_vel = jnp.array([0., 0., 0.])
+    qpos = jnp.concatenate([ball_xy, ball_z, ball_quat], axis=0)
+    qvel = jnp.concatenate([ball_vel, ball_ang_vel], axis=0)
+    target_pos = jnp.array([0., 0., 0.])
     qpos = dx.qpos.at[:].set(qpos)
     qvel = dx.qvel.at[:].set(qvel)
     mocap_pos = dx.mocap_pos.at[:].set(target_pos)
@@ -122,7 +100,7 @@ def set_data(mx: mjx.Model, dx: mjx.Data, ctx: Context, key: jnp.ndarray) -> mjx
 
 def gen_network(seed: int) -> Network:
     key = jax.random.PRNGKey(seed)
-    return Policy([12, 128, 128, _cfg.mx.nv], key)
+    return Policy([13, 32, 32, _cfg.mx.nv], key)
 
 def is_terminal(mx: mjx.Model, dx: mjx.Data) -> jnp.ndarray:
     time_limit =  (dx.time/ mx.opt.timestep) > (_cfg.ntotal - 1)
