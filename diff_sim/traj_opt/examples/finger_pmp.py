23b1e6c2aae0d68577be082b67197aca0ce208ff
Daniel Layeghi <Daniellayeghi@gmail.com>
2024-12-13 16:27:49 +0000

    2d control finger

--- diff_sim/traj_opt/examples/finger_pmp.py
+++ diff_sim/traj_opt/examples/finger_pmp.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     dx = mjx.make_data(mx)
     dx = jax.tree.map(upscale, dx)
     qpos_init = jnp.array([-.8, 0, -.8])
-    Nsteps, nu = 300, 1
+    Nsteps, nu = 300, 2
     U0 = jax.random.normal(jax.random.PRNGKey(0), (Nsteps, nu)) * 2
 
     def set_control(dx, u):
