bb1aa5ec52836cc609547a6dd5577fa12b0674ae
thomascbrs <thomas.corberes@gmail.com>
2025-01-20 17:12:08 +0000

    Update initial condition in policy debug arm

--- diff_sim/traj_opt/examples/finger_policy.py
+++ diff_sim/traj_opt/examples/finger_policy.py
@@ -10,6 +10,8 @@ from diff_sim.traj_opt.policy import (
     simulate_trajectories, make_loss_multi_init, make_step_fn, build_fd_cache
 )
 from diff_sim.nn.base_nn import Network
+from diff_sim.utils.math_helper import angle_axis_to_quaternion, quaternion_to_angle_axis
+
 
 def upscale(x):
     """Convert data to 64-bit precision."""
@@ -19,6 +21,46 @@ def upscale(x):
         elif x.dtype == jnp.float32:
             return jnp.float64(x)
     return x
+
+def generate_inital_conditions(key):
+    # Solution of IK
+    _, key = jax.random.split(key)
+    sign = 2.*jax.random.bernoulli(key, 0.5) - 1.
+
+    # Reference target position in spinner local frame R_s
+    _, key = jax.random.split(key, num=2)
+    theta_l = jax.random.uniform(key, (1,), minval=0.55, maxval=2.55) # Polar Coord
+    l_spinner = 0.22
+    # r = jax.random.uniform(key, (1,), minval=l_spinner + 0.01, maxval=l_spinner + 0.01)
+    r_l = l_spinner
+    x_s,y_s = r_l*jnp.cos(theta_l), r_l*jnp.sin(theta_l) # Cartesian in R_l
+    
+    # Reference target position in finger frame R_f
+    x, y = x_s, y_s - 0.39
+
+    # Inverse kinematic formula
+    l1, l2 = 0.17, 0.161
+    q1 = sign * jnp.arccos( (x**2 + y**2 - l1**2 - l2**2)/(2*l1*l2) )
+    q0 = jnp.arctan2(y,x) - jnp.arctan2(l2 * jnp.sin(q1), l1 + l2*jnp.cos(q1))
+
+    # dx = dx.replace(qpos=dx.qpos.at[0].set(q0[0]))
+    # dx = dx.replace(qpos=dx.qpos.at[1].set(q1[0]))
+
+    # Set Mocap quaternion
+    _, key = jax.random.split(key)
+    theta_cap = jax.random.uniform(key, (1,), minval=0., maxval=3.14)
+    axis = jnp.array([0, -theta_cap[0], 0])
+    quat = angle_axis_to_quaternion(axis)
+    # dx = dx.replace(mocap_quat=dx.m
+    #                 ocap_quat.at[:].set(quat))
+    pos = jnp.array([-0.2, 0., -0.4])
+    # dx = dx.replace(mocap_pos=dx.mocap_pos.at[:].set(pos))
+
+    _, key = jax.random.split(key, num=2)
+    theta = jax.random.uniform(key, (1,), minval=-0.9, maxval=0.9)
+    # dx = dx.replace(qpos=dx.qpos.at[2].set(theta[0]))
+
+    return jnp.concatenate([q0,q1,theta])
 
 if __name__ == "__main__":
 
@@ -30,15 +72,18 @@ if __name__ == "__main__":
 
     # Suppose we define a batch of initial conditions
     # e.g. 5 different qpos, qvel
-    qpos_inits = jnp.array([
-        [-0.8,  0.0, -0.8]
-    ])
-    # repeat qpos_inits to match to size 164
-    qpos_inits = jnp.repeat(qpos_inits, 64, axis=0)
-    qpos_inits += 0.01 * jax.random.normal(jax.random.PRNGKey(0), qpos_inits.shape)
+    # qpos_inits = jnp.array([
+    #     [-0.8,  0.0, -0.8]
+    # ])
+    # # repeat qpos_inits to match to size 164
+    # qpos_inits = jnp.repeat(qpos_inits, 64, axis=0)
+    # qpos_inits += 0.01 * jax.random.normal(jax.random.PRNGKey(0), qpos_inits.shape)
+    init_key = jax.random.PRNGKey(10) 
+    keys = jax.random.split(init_key, 2)  # Generate 100 random keys
+    qpos_inits = jax.vmap(generate_inital_conditions, in_axes=(0))(keys)
     qvel_inits = jnp.zeros_like(qpos_inits)  # or any distribution you like
 
-    Nsteps, nu = 300, 2
+    Nsteps, nu = 100, 2
 
     def running_cost(dx):
         pos_finger = dx.qpos[2]
@@ -89,13 +134,13 @@ if __name__ == "__main__":
 
     # Create your policy net, optimizer, and do gradient descent
     nn = PolicyNet([6, 64, 64, 2], key=jax.random.PRNGKey(0))
-    adam = optax.adamw(8e-4)
+    adam = optax.adamw(8.e-4)
     opt_state = adam.init(equinox.filter(nn, equinox.is_array))
 
     # Same "Policy" class as before
     from diff_sim.traj_opt.policy import Policy
     optimizer = Policy(loss=loss_fn)
-    optimal_nn = optimizer.solve(nn, adam, opt_state, max_iter=75)
+    optimal_nn = optimizer.solve(nn, adam, opt_state, max_iter=200)
 
     fd_cache = build_fd_cache(dx_template)
     step_fn = make_step_fn(mx, set_control, fd_cache)
