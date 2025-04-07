1cd1af0bba52514d61ffdec57ff8c3c8f44d6578
thomascbrs <thomas.corberes@gmail.com>
2025-04-07 15:42:58 +0100

    Introduce Context. finger_pmp script updated

---
+++ diff_sim/optim/meta_context.py
@@ -0,0 +1,39 @@
+import jax
+import jax.numpy as jnp
+import mujoco
+from dataclasses import dataclass
+from typing import Callable, Optional, Set
+from mujoco import mjx
+
+
+@jax.tree_util.register_static
+@dataclass(frozen=True)
+class Context:
+    # Callbacks
+    gen_model: Callable[[], mujoco.MjModel]
+    running_cost: Callable[[mjx.Model, mjx.Data], jnp.ndarray]
+    terminal_cost: Callable[[mjx.Model, mjx.Data], jnp.ndarray]
+    set_control: Callable[[mjx.Data, jnp.ndarray], mjx.Data]
+
+    # Configuration
+    lr: float  # learning rate
+    nsteps: int  # total episode length
+    epochs: int  # training epochs
+    mx: mjx.Model  # MJX model
+    num_gpu: Optional[int] = 1  # number of devices
+    seed: Optional[int] = 0  # random seed
+    batch: Optional[int] = 1  # batch size
+
+    # fd parameters
+    ctrl_dim: Optional[int] = 0  # Dimension of the control (not necessarily dx.ctrl)
+    target_fields: Optional[Set[str]] = None  # Target fields for finite differences
+    eps: Optional[float] = 1e-6  # Eps for finite differences
+
+    # ilqr parameters
+    ddp: Optional[int] = False  # DDP (use 2nd derivatives)
+    reg: Optional[float] = 1e-6  # Regularisation term to Q_uu
+
+    def __post_init__(self):
+        assert (
+            self.num_gpu <= jax.device_count()
+        ), "num_gpu cannot exceed number of available devices."
--- diff_sim/optim/scripts/finger_pmp_index.py
+++ diff_sim/optim/scripts/finger_pmp_index.py
@@ -1,66 +1,78 @@
+import os
 import jax
 import jax.numpy as jnp
+
 jax.config.update("jax_enable_x64", True)
-jax.config.update('jax_default_matmul_precision', 'high')
+jax.config.update("jax_default_matmul_precision", "high")
 import mujoco
 from mujoco import mjx
 from diff_sim.optim.simulation.fd_cache import build_fd_cache
 from diff_sim.optim.pmp_fd_indexes import PMP, make_loss_fn
 from diff_sim.utils.mj_viewers import visualise_traj_generic
+from diff_sim.optim.meta_context import Context
+from diff_sim.optim.simulation.step import make_step_fn, make_step_fn_fd
 
+# Compilation option
 jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
-jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
-# jax.devices("cpu")[0]  # Get the CPU device
+jax.config.update(
+    "jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir"
+)
 
 
-def upscale(x):
-    """Convert data to 64-bit precision."""
-    if hasattr(x, 'dtype'):
-        if x.dtype == jnp.int32:
-            return jnp.int64(x)
-        elif x.dtype == jnp.float32:
-            return jnp.float64(x)
-    return x
+model_path = os.path.join(os.path.dirname(__file__), "../xmls/finger_mjx.xml")
+def gen_model() -> mujoco.MjModel:
+    return mujoco.MjModel.from_xml_path(model_path)
+
 
 if __name__ == "__main__":
-    model = mujoco.MjModel.from_xml_path("xmls/finger_mjx.xml")
+    with jax.default_device(jax.devices("cpu")[0]):
 
-    with (jax.default_device(jax.devices("cpu")[0])):
-        mx = mjx.put_model(model)
-        dx = mjx.make_data(mx)
-        dx = jax.tree.map(upscale, dx)
+        model = gen_model()
         d = mujoco.MjData(model)
 
-        qpos_init = jnp.array([-.8, 0, -.8])
+        qpos_init = jnp.array([-0.8, 0, -0.8])
         Nsteps, nu = 300, 2
-        U0 = 0.2*jax.random.normal(jax.random.PRNGKey(0), (Nsteps, nu)) * 2
+        U0 = 0.2 * jax.random.normal(jax.random.PRNGKey(0), (Nsteps, nu)) * 2
 
         def running_cost(dx):
             pos_finger = dx.qpos[2]
             u = dx.ctrl
-            return 0.002 * jnp.sum(u ** 2) + 0.001 * pos_finger ** 2
+            return 0.002 * jnp.sum(u**2) + 0.001 * pos_finger**2
 
         def terminal_cost(dx):
             pos_finger = dx.qpos[2]
-            return 4 * pos_finger ** 2
+            return 4 * pos_finger**2
 
         def set_control(dx, u):
             return dx.replace(ctrl=dx.ctrl.at[:].set(u))
 
-        fd_cache = build_fd_cache(
-            mx, dx, ('qpos', 'qvel'), 2
+        # 1) General context for the optimisation
+        ctx = Context(
+            lr=0.5,
+            nsteps=300,
+            epochs=10,
+            mx=mjx.put_model(gen_model()),
+            gen_model=gen_model,
+            running_cost=running_cost,
+            terminal_cost=terminal_cost,
+            set_control=set_control,
+            ctrl_dim=2,
+            target_fields={"qpos", "qvel"},
+            eps=1e-6,
         )
+
+        # 2) Select a step function (Implicit, FD or AD)
+        # step_fn = make_step_fn(ctx) # Implicit
+        step_fn = make_step_fn_fd(ctx)  # FD
+        # TODO : AD
 
         # 3) Build the loss function with the new step fn
         loss_fn = make_loss_fn(
-            mx=mx,
             qpos_init=qpos_init,
-            set_ctrl_fn=set_control,
-            running_cost_fn=running_cost,
-            terminal_cost_fn=terminal_cost,
-            fd_cache=fd_cache,
+            step_fn=step_fn,
+            ctx=ctx,
         )
 
         # l, x = loss_fn(U0)
@@ -69,7 +81,7 @@ if __name__ == "__main__":
 
         # 4) Optimize
         pmp = PMP(loss=lambda u: loss_fn(u)[0])
-        optimal_U = pmp.solve(U0=U0, learning_rate=0.5, max_iter=10)
+        optimal_U = pmp.solve(U0=U0, learning_rate=ctx.lr, max_iter=ctx.epochs)
 
         l, x = loss_fn(optimal_U)
 
